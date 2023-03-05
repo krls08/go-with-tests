4f9f8a4e9acd423cf3b95a8cd45fcae0c93a537b
Carles Pujades <carles.pujades@mca-groupe.com>
2023-03-05 21:20:49 +0100

    testing depencendy injection

---
+++ 07_Dependency_injection/di.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+func Greet(writer io.Writer, name string) {
+	//func Greet(writer *bytes.Buffer, name string) {
+	//fmt.Printf("Hello, %s", name)
+	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+
+func main() {
+	Greet(os.Stdout, "Krls")
+
+	log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(MyGreeterHandler)))
+
+}
