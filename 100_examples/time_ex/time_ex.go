66fb02876aa7731afb6a5bf2b66b93dc83738ba2
Carles Pujades <carles.pujades@mca-groupe.com>
2023-03-20 03:16:37 +0100

    time until

--- 100_examples/time_ex/time_ex.go
+++ 100_examples/time_ex/time_ex.go
@@ -18,4 +18,18 @@ func main() {
 	nt := t.Add(td)
 	fmt.Println("t =>", nt)
 
+	fmt.Println("-----------------")
+	tnow := time.Now()
+	fmt.Println("time now:", tnow)
+	nowPlus8 := time.Now().Add(8 * time.Minute)
+	nowMinus8 := time.Now().Add((-8) * time.Minute)
+	fmt.Println("time until +8:", time.Until(nowPlus8))
+	fmt.Println("time until -8:", time.Until(nowMinus8))
+	if time.Until(nowPlus8) < 9*time.Minute {
+		fmt.Println(tnow, "is less than 9 min below", nowPlus8)
+	}
+	if time.Until(nowMinus8) < 0 {
+		fmt.Println(nowMinus8, "is less than 0 min until", tnow)
+	}
+
 }
