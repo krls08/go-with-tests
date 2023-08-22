dc578207d8de6907456b1fbca6c43330d195e0b8
krlspj <krls.pjds@gmail.com>
2023-08-22 15:10:29 +0200

    PlayerServer test

--- 23_APP/24_HTTP_server/server.go
+++ 23_APP/24_HTTP_server/server.go
@@ -1,7 +1,25 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
 
 func PlayerServer(w http.ResponseWriter, r *http.Request) {
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+
+	//fmt.Printf("### url path: %s\n", r.URL.Path)
+	//fmt.Printf("### player: %s\n", player)
+
+	if player == "Pepper" {
+		fmt.Fprint(w, "20")
+		return
+	}
+
+	if player == "Floyd" {
+		fmt.Fprint(w, "10")
+		return
+	}
 
 }
