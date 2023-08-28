9b31ea2b4344c2cff0ad6c3b17caed825eb68a0b
krlspj <krls.pjds@gmail.com>
2023-08-29 00:56:43 +0200

    test FileSystemStore with ReadWriteSeeker

--- 23_APP/26_IO_sorting/file_system_store.go
+++ 23_APP/26_IO_sorting/file_system_store.go
@@ -5,11 +5,23 @@ import (
 )
 
 type FileSystemPlayerStore struct {
-	database io.ReadSeeker
+	database io.ReadWriteSeeker
 }
 
 func (f *FileSystemPlayerStore) GetLeague() []Player {
 	f.database.Seek(0, 0)
 	league, _ := NewLeague(f.database)
 	return league
+}
+
+func (f *FileSystemPlayerStore) GetPlayerScore(name string) int {
+	var wins int
+
+	for _, player := range f.GetLeague() {
+		if player.Name == name {
+			wins = player.Wins
+			break
+		}
+	}
+	return wins
 }
